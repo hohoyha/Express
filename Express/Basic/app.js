414a09c9a4d7caf4e7dd811bbf31cf73ba7ceec3
DESKTOP-IV6M0AG\Myhoho <hohoyha@hotmail.com>
2017-03-30 17:01:25 +0900

    static html -> code add

--- Express/Basic/app.js
+++ Express/Basic/app.js
@@ -2,7 +2,20 @@ var express = require('express');
 var app = express();
 
 app.get('/hello', function(req, res){
-  res.send('hello world');
+   res.send('hello world');
+});
+
+app.get('/staticTest', function(req, res){
+  var output = `<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+ <h2> Static Test </h2>
+</body>
+</html>
+`
+  res.send(output);
 });
 
 app.use(express.static('public'));
