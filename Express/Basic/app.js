1af16629fb87a7bf775843a46d5d4c1205548a3b
DESKTOP-IV6M0AG\Myhoho <hohoyha@hotmail.com>
2017-03-30 16:55:40 +0900

    static file added

--- Express/Basic/app.js
+++ Express/Basic/app.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
+app.get('/hello', function(req, res){
   res.send('hello world');
 });
+
+app.use(express.static('public'));
 
 app.listen(3000);
