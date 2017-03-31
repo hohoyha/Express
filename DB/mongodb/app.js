6fe9db6981fb2fdbbec133ba624ac05168fc2c25
DESKTOP-IV6M0AG\Myhoho <hohoyha@hotmail.com>
2017-03-31 19:36:03 +0900

    mongodb startkit

---
+++ DB/mongodb/app.js
@@ -0,0 +1,34 @@
+// app.js
+ 
+// [LOAD PACKAGES]
+var express     = require('express');
+var app         = express();
+var bodyParser  = require('body-parser');
+var mongoose    = require('mongoose');
+var Book    = require('./models/books')
+ 
+// [CONFIGURE APP TO USE bodyParser]
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+ 
+// [CONFIGURE SERVER PORT]
+var port = process.env.PORT || 4000;
+ 
+// [CONFIGURE ROUTER]
+var router = require('./routes')(app);
+
+var db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', function(){
+    // CONNECTED TO MONGODB SERVER
+    console.log("Connected to mongod server");
+});
+ 
+mongoose.connect('mongodb://localhost/mongodb_tutorial');
+
+
+ 
+// [RUN SERVER]
+var server = app.listen(port, function(){
+ console.log("Express server has started on port " + port)
+});
